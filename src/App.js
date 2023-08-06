95db4f951f22077d798bdf63652e104f36316cfb
Pavan <pansgami@gmail.com>
2023-08-06 19:11:43 +0530

    Add search feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from 'react'
+import './App.css'
+import MovieCard from './MovieCard'
 function App() {
+  const API_URL = 'http://www.omdbapi.com/?apikey=46ab70c&s=movies&page=1'
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.Search))
+  }, [])
+  const [movies, setMovies] = useState([])
+  const [searchQuery, setSearchQuery] = useState('')
+
+  // useEffect(() => {
+  //   const url = 'http://www.omdbapi.com/?apikey=46ab70c&s=movies&page=1'
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const promises = data.Search.map((movie) => {
+  //         const url = `http://www.omdbapi.com/?apikey=46ab70c&i=${movie.imdbID}`
+  //         return fetch(url).then((response) => response.json())
+  //       })
+  //       return Promise.all(promises)
+  //     })
+  //     .then((movies) => setMovies(movies))
+  // }, [])
+  console.log(movies)
+  const apiKey = '46ab70c'
+  const searchUrl = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchQuery}`
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    fetch(searchUrl)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.Search))
+  }
+  const handleChange = (e) => {
+    e.preventDefault()
+    setSearchQuery(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='search_nav'>
+        <div className='title'>
+          <h1>Movie</h1>
+        </div>
+
+        <div className='search_box'>
+          <form onSubmit={handleSearch}>
+            <input value={searchQuery} onChange={handleChange} />
+            <button>Search </button>
+          </form>
+        </div>
+      </div>
+      <div className='movies_continer'>
+        {movies ? (
+          movies.map((movie) => <MovieCard movie={movie} />)
+        ) : (
+          <h2>Movie not found</h2>
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/MovieCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './MovieCard.css'
+function MovieCard({ movie }) {
+  return (
+    <div className='card'>
+      <div className='poster'>
+        <img
+          src={
+            movie.Poster
+              ? movie.Poster
+              : 'https://unsplash.com/photos/q8P8YoR6erg'
+          }
+        />
+      </div>
+      <div className='info'>
+        <p className='title'>{movie.Title}</p>
+        <p className='vote'>{movie.imdbRating}</p>
+      </div>
+      <div className='overview'>
+        <h2 className='overview_title'>OverView:</h2>
+        <p className='overview_info'>{movie.Plot}</p>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
